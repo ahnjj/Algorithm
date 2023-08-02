13a544e8bfcfc17949d5448e1649b00a95f3aff9
Stability_DEV <dkswjd0909@gmail.com>
2023-08-02 14:41:15 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,3 +1,9 @@
 def solution(number):
-    answer = 0
-    return answer
+    cnt = 0
+    for i in range(len(number)-2):
+        for j in range(i+1,len(number)-1):
+            for x in range(j+1,len(number)):
+                if -(number[i] + number[j]) == number[x]:
+                    cnt += 1
+
+    return cnt
