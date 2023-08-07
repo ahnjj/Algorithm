a7bdfe5f7d9cb6cf69951eb438f8408da8787527
Ahnjj_DEV <dkswjd0909@gmail.com>
2023-08-07 15:20:17 +0900

    [unrated] Title: 달리기 경주, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,15 @@
+def solution(players, callings):
+
+    cur_players = {v : i+1 for i,v in enumerate(players)}
+    cur_rank = {rank : player for player, rank in cur_players.items()}   
+
+    for i in callings:
+        former = cur_rank[cur_players[i] - 1]
+        cur_rank[cur_players[i] - 1] , cur_rank[cur_players[i]] = cur_rank[cur_players[i]] , cur_rank[cur_players[i] -1]
+        cur_players[former], cur_players[i] = cur_players[i], cur_players[former]
+
+
+
+    return list(dict(sorted(cur_rank.items())).values())
+
+   
