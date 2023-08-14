3b3e9658018ab874c7a423e6550c20d94f833704
Ahnjj_DEV <dkswjd0909@gmail.com>
2023-08-14 12:13:32 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.20 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,35 +1,16 @@
 def solution(new_id):
     ans = ''
-    tmp = ''
-    # 1
+    import re
     new_id = new_id.lower()
-
-    # 2
-    for i in new_id:
-        if i.isalpha() or i.isdigit() or i in ('-','_', '.'):
-            tmp += i
-
-
-    # 3
-    for idx in range(len(tmp)):
-        if tmp[idx:idx+2] == '..':
-            ans += '.'
-        else:
-            ans += tmp[idx]
-        ans = ans.replace('..','.')
-
-    # 4
-    ans = ans.strip('.')
-
-    # 5
+    ans =re.sub('[^a-z0-9\-_.]','', new_id)
+    ans = re.sub('\.+','.',ans)
+    ans = re.sub('^[.]','',ans)
     if ans == '':
         ans += 'a'
 
-    # 6
     ans = ans[:15]
-    ans = ans.rstrip('.')
+    ans = re.sub('[.]$','',ans)
 
-    # 7
     if len(ans) <= 2:
         ans += ans[-1]*(3-len(ans))
 
