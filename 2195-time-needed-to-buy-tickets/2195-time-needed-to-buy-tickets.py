0bba3a27ff942a42c086a06daf73d437f00fc4de
Ahnjj_DEV <dkswjd0909@gmail.com>
2023-08-15 14:17:43 +0900

    Time: 70 ms (30.44%), Space: 16.5 MB (32.37%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
@@ -1,17 +1,12 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        q = collections.deque()
-        goal = tickets[k]
+        q = []
+
         while any(tickets):
             for idx, t in enumerate(tickets):
                 if t != 0:
                     q.append(idx)
                     tickets[idx] -= 1
 
-        cnt = 0
-        line = len(q)
-        while cnt != goal:
-            if q.popleft() == k:
-                cnt += 1
-                if cnt == goal:
-                    return line - len(q)
+        final_index = max([idx for idx, p in enumerate(q) if p == k])
+        return final_index+1
