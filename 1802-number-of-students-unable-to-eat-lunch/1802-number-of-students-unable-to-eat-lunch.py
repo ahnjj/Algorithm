1c68cf3969b5e791f5fa187d9c5007bd61566f3d
Ahnjj_DEV <dkswjd0909@gmail.com>
2023-08-16 15:41:29 +0900

    Time: 44 ms (80.89%), Space: 16.4 MB (40.73%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.py
@@ -1,6 +1,6 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        while sandwiches:
+        while sandwiches or students:
             if sandwiches[0] in students:
                 want = students.pop(0)
                 if want == sandwiches[0]:
