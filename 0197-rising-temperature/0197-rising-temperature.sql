d9393251ae5c9f1491f40b53466b9aa8b5a17e50
Ahnjj_DEV <dkswjd0909@gmail.com>
2023-08-26 21:00:11 +0900

    Delete 0197-rising-temperature directory

--- 0197-rising-temperature/0197-rising-temperature.sql
+++
@@ -1,6 +0,0 @@
-SELECT Today.id
-FROM Weather AS Yesterday
-     INNER JOIN Weather AS Today ON DATE_ADD(yesterday.recordDate, INTERVAL 1 DAY) = Today.recordDate
-WHERE Yesterday.temperature < Today.temperature
-
-     
