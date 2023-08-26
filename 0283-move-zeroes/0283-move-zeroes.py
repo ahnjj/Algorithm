a6148ea82e272f83207ce5b80777ccfa081751db
Ahnjj_DEV <dkswjd0909@gmail.com>
2023-08-26 21:00:37 +0900

    Delete 0283-move-zeroes directory

--- 0283-move-zeroes/0283-move-zeroes.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        ptr0 = 0
-        for ptrn in range(len(nums)):
-            if nums[ptrn] != 0:
-                nums[ptr0], nums[ptrn] = nums[ptrn], nums[ptr0]
-                ptr0 += 1
-
-        return nums
