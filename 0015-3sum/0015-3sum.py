97e144e9c2b573eec266d3d19fc1211d4a7ceeb9
Ahnjj_DEV <dkswjd0909@gmail.com>
2023-08-28 09:16:51 +0900

    Time: 637 ms (97.49%), Space: 20.9 MB (17.28%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,24 +1,39 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        zero = 0
+        n = []
+        p = []
+        
+        for num in nums:
+            if num < 0:
+                p.append(num)
+            elif num > 0:
+                n.append(num)
+            else:
+                zero += 1
+
         output = set()
-        nums.sort()
-        n = len(nums)
+        N = set(n)
+        P = set(p)
 
-        for i in range(0,n-2):
-            if nums[i] == nums[i-1] and i > 0:
-                continue
-            j = i+1
-            k = n-1
-            while j < k: 
-                if nums[j] + nums[k] == -(nums[i]):
-                    output.add((nums[i], nums[j], nums[k]))
-                    k -= 1
-                    j += 1
-                elif  nums[j] + nums[k] > -(nums[i]):
-                    k -= 1
-                elif  nums[j] + nums[k] < -(nums[i]):
-                    j += 1
-            
+        if zero:
+            for num in N:
+                if (-1)*num in P:
+                    output.add((0, num, (-1)*num))
+        if zero >= 3:
+            output.add((0,0,0))
 
+
+        for i in range(len(n)):
+            for j in range(i+1,len(n)):
+                target = -1*(n[i] + n[j])
+                if target in P:
+                    output.add(tuple(sorted([n[i],n[j],target])))
             
+        for i in range(len(p)):
+            for j in range(i+1,len(p)):
+                target = -1*(p[i]+p[j])
+                if target in N:
+                    output.add(tuple(sorted([p[i],p[j],target])))
+
         return output 
