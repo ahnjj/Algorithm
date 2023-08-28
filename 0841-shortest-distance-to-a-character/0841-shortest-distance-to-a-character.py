df5c00d85731948d4313b691af5b817eb0483f5b
Ahnjj_DEV <dkswjd0909@gmail.com>
2023-08-28 22:56:47 +0900

    Time: 44 ms (88.74%), Space: 16.4 MB (36.43%) - LeetHub

--- 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.py
+++ 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.py
@@ -1,19 +1,26 @@
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
-        befc = float('-inf')
+        a, n = [], len(s)
         output = []
 
-        for i, x in enumerate(s):
-                if x == c:
-                        befc = i
-                output.append(i-befc)
-
-        befc = float('inf')
-        for i in range(len(s)-1, -1, -1):
+        # 특정원소 일단 싹 찾고 배열a에 위치 넣기
+        for i in range(n):
                 if s[i] == c:
-                        befc = i
-                output[i] = min(output[i], befc-i)
-        
+                        a.append(i)
+
+
+        # 2 pointer
+        j = 0
+        for i in range(n):
+                if s[i] == c:
+                        output.append(0)
+                        j += 1
+                elif i < a[0]:      # 첫번째 c보다 앞에 있는 원소들
+                        output.append(a[0] - i)
+                elif i > a[-1]:     # 마지막 c보다 뒤에 있는 원소들
+                        output.append(i-a[-1])
+                else:
+                        output.append(min((i-a[j-1]), (a[j] - i)))
+
         return output
-                
         
